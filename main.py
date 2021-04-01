b3de48f045906f3688df2dd4918ff1bcb53f81e9
Filippo011235 <fadamcewicz1@gmail.com>
2021-04-01 22:49:42 +0200

    Menu handling functions v0.9

--- main.py
+++ main.py
@@ -1,42 +1,87 @@
 import math
-from math import sin, cos, exp, sqrt, pi, log
-from task_function import TaskFunction
-from os import system
-from os import listdir
+from math import sin, cos, exp, sqrt, pi, log   # Instead of 'math.sin' -> 'sin'
+from task_function import TaskFunction # Class handling task/function data
+from os import system, listdir
+# from menu import handle_menu_input, display_menu # waits for extra program modules
 
-# Initialization
-quit_bool = False
-filesDir = './Templates/'
-currentFunction = None
+# I use those interchangeably: 
+# task - function - file
+# currentFunction - default - currFnc
 
-fncDatabase = []
-fncDisplay = {} # For switching between templates
 
-for file in listdir(filesDir):
+
+####### Initialization #######
+
+
+quit_bool = False           # For exiting the main loop
+filesDir = './Templates/'   # Directory with templates functions .txt files
+currentFunction = None      # Fnc which you will be working on
+
+fncDatabase = []        # Stores fnc in current session
+fncDisplay = {}         # For displaying/switching between templates
+
+# Read all fnc into the fncDatabase
+for file in listdir(filesDir): 
     if not currentFunction: # make the first file a default function
         currentFunction = TaskFunction(filesDir + file)
 
-    fncDatabase.append(TaskFunction(filesDir + file))
+    # Database of TaskFunction class objects
+    fncDatabase.append(TaskFunction(filesDir + file)) 
+    # Display tries to keeps up; Stores task name and equation
     fncDisplay[len(fncDatabase)] = [fncDatabase[-1].fncName, fncDatabase[-1].fnc]
 
-# system('pause')
-    
-def change_current_parameters():
+
+
+####### Various cool functions (which should be in seperate modules) #######
+
+
+def change_current_parameters(): 
+    """Change parameters of the current task."""
+    global currentFunction
     system('cls')
-    print('chaning parameters')
+
+    print('Jakie parametry, aktualnego zadania, chcesz zmienić?')
+    print('(Uwaga zmiany działają tylko podczas tej sesji!)')
+    print('1 - Wzór')
+    print('     ', currentFunction.fnc)
+    print()
+    print('2 - Komentarz')     # Yup, has to be on nr. 2, look below
+    print('     ', currentFunction.scoreComment)
+    print()
+    print('(Zakres podaj jako dwa int, rozdzielone spacją)')
+    for i in range(len(currentFunction.x)):
+        print(3+i, '- zakres x[',i,']: ', currentFunction.x[i])
+    print()
+
+    fncParameter = int(input('Wybrana opcja: ')) # get the input and make it an int
+
+    # Primitive switch for updating the currFnc
+    # ranges of x[n] have to be for fncParameter >= 3 to handle varying n dimension
+    if fncParameter == 1:
+        currentFunction.fnc = input('Nowy wzór f(x): ')
+    elif fncParameter == 2:
+        currentFunction.scoreComment = input('Nowy komentarz f(x): ')
+    elif fncParameter >= 3:
+        idx = fncParameter-3 # idx is a subset of 0..4
+        input_text = 'Nowy zakres x[' + str(idx) + ']: '
+        new_x = list(map(int, input(input_text).split()))
+        currentFunction.x[idx] = new_x
+    else:
+        print('Zly nr opcji :(')
     system('pause')
 
 def change_to_template():
+    """Change currentFunction to one of templates"""
     global currentFunction
     system('cls')
     print('Wybierz funckję zadania do rozwiązania: ')
     for k, v in fncDisplay.items():
         print(k, ' - ', v[0])
-        # print('   ', v[1])                      
+        # print('   ', v[1])  # Don't reckon equation is necessary 
     
-    newFncNr = int(input('Wybrana funkcja: ')) # get the input and make it an int
+    newFncNr = int(input('Wybrana funkcja: '))
 
-    if newFncNr in fncDisplay: # does input exist as a key in the fnc dictionary
+    if newFncNr in fncDisplay: # Checks whether input exist as a key in the fnc dictionary
         currentFunction = fncDatabase[newFncNr-1]
     else:
         print('Zly nr funkcji :(')
@@ -47,27 +92,71 @@ def change_to_template():
     # system('pause')
 
 def change_to_test():
+    """Quickly change currFnc to a simple, test one, from the first file."""
+    global currentFunction
     system('cls')
-    print('chaning to test')
-    system('pause')
+    # Assumes the first fnc/file is the test one
+    currentFunction = fncDatabase[0] 
 
 def change_to_manual():
+    """Manually enter new task, also creating new file. Update currFnc"""
+    global currentFunction, fncDatabase
     system('cls')
-    print('chaning to manual')
-    system('pause')
+    
+    fileName = input('Podaj nazwę zadania(pliku bez ".txt"): ')
+    fullFilePath = filesDir + fileName + '.txt'
+    newEquation = input('Podaj wzór funkcji celu: ')
+    variableQnt = int(input('Podaj ile jest zmiennych x[n]: '))
+    rangeInfoLine = str()
+    for n in range(variableQnt):
+        inputInfo = 'Podaj zakres wartości dla x[' + str(n) + '], (np. "-5 5"): '
+        rangeInfoLine = rangeInfoLine + input(inputInfo) + ' '
+    
+    newComment = input('Podaj komentarz, wynik itp.: ')
+    
+    outF = open(fullFilePath, 'w')
+    # Write data to output file
+    outF.write(newEquation + '\n')
+    outF.write(rangeInfoLine + '\n')
+    outF.write(newComment)
+    outF.close()    # Always clean after yourself!
+
+    # If everything checks out(or even when not) 
+    # add new class to the Database and Display
+    fncDatabase.append(TaskFunction(fullFilePath))
+    fncDisplay[len(fncDatabase)] = [fncDatabase[-1].fncName, fncDatabase[-1].fnc]
+    # Update the currFnc
+    currentFunction = fncDatabase[-1]
+
+    # system('pause')
 
 def calculate_task():
+    """ 2nd Stage of Project: calculations"""
+    global currentFunction
     system('cls')
-    print('calculate_task')
+
+    x = []      # something for eval() to work on
+    print('Podaj wartosci x dla ktorych trza policzyc: ')
+    for i in range(len(currentFunction.x)):
+        inputInfo = 'Wartość x[' + str(i) + ']: '
+        x.append(float(input(inputInfo)))
+    
+    y=eval(currentFunction.fnc)
+    print(y, ' B) ')
+    
     system('pause')
 
 def quit_program():
-    # system('cls')
-    global quit_bool # explicite adress global, not local, variable
+    """Quit main loop using bool variable"""
+    global quit_bool 
 
-    # print('quit_program')
     quit_bool = True
-    # system('pause')
+
+
+
+####### Menu Functions #######
+
+
 
 def invalid_menu_input():
     system('cls')
@@ -76,6 +165,8 @@ def invalid_menu_input():
 
 
 def handle_menu_input(argument):
+    """Based on argument(int), use functions. Hardwired with display_menu()!"""
+    # Dictionary of options
     menu_functions = {
         '1': change_current_parameters,   # '1 - Zmień parametry aktualnego zadania'
         '2': change_to_template,           # '2 - Zmień f. celu na gotowy szablon'
@@ -85,38 +176,38 @@ def handle_menu_input(argument):
         '6': quit_program,                # '6 - Wyjdź'
     }
     # Get the function from menu_functions dictionary
-    # menu_functions.get(argument, invalid_menu_input())()
+    # niewypał: menu_functions.get(argument, invalid_menu_input())()
+    # wypał:
     func = menu_functions.get(argument)
     if func:
-        # Execute the function
-        func()
+        func() # Execute the function
     else:
-        invalid_menu_input()
+        invalid_menu_input() # such func() doesn't exist!
 
 
-def display_menu():
+def display_menu(currentFnc):
+    """Displays the menu. Hardwired with handle_menu_input()!"""
     system('cls')
     print('Aktualne zadanie:')
     print('-----------------------------------------------')
-    currentFunction.displayData()
+    currentFnc.displayData()
     print('-----------------------------------------------')
     print('1 - Zmień parametry aktualnego zadania')
     print('2 - Zmień f. celu na gotowy szablon')
     print('3 - Zmień f. celu na testową(f. kwadratowa)')
-    print('4 - Wpisz ręcznie nową f. celu')
+    print('4 - Wpisz ręcznie nową f. celu(utwórz plik)')
     print('5 - Policz')
     print('6 - Wyjdź')
     print()
 
 
-if __name__ == '__main__':
 
-    while(not quit_bool):
-        display_menu()
-        menu_option = input('Wybór: ')
-        system('cls')
-        handle_menu_input(menu_option)
+####### The Holy Main #######
 
 
-        
+while(not quit_bool):
+    display_menu(currentFunction)
+    menu_option = input('Wybór: ')
+    system('cls')
+    handle_menu_input(menu_option)
 
--- task_function.py
+++ task_function.py
@@ -1,27 +1,28 @@
 class TaskFunction:
-    'Stores optimization function, data'
-    # This is our initializer method when a new object is created.
+    """Stores optimization function/task data"""
+    
     def __init__(self, filePath):
-        taskData = []
-
-        try:
+        taskData = [] # Collects data as strings
+        
+        try:    # ... opening the file
             with open(filePath) as f:
                 for line in f:
                     taskData.append(line)
         except IOError:
             print('Error: File does not appear to exist: ', filePath)
 
-        self.fncName = filePath[12:-4]     # use online middle of './Templates/XXX.txt' 
-        self.fnc = taskData[0][0:-1] # [0:-1] removes \n at end of line 
+        # Function/File name; use online middle of './Templates/XXX.txt' 
+        self.fncName = filePath[12:-4] 
+        # Funcja celu   
+        self.fnc = taskData[0][0:-1] # [0:-1] removes \n at end of line
+        # Zakresy wartości, as single integers
         xRanges = list(map(int, taskData[1].split()))
         self.x = []
-        for i in range(0, len(xRanges), 2):   # get pairs of x[i] min and max
+        for i in range(0, len(xRanges), 2):   # get pairs of x[i] min and max values
             self.x.append([xRanges[i], xRanges[i+1]])   # x[i] = [x_imin, x_imax]
+        # Any comment about fnc, it's opti score etc.
         self.scoreComment = taskData[2][0:-1] # [0:-1] removes \n at end of line
     
-    # def __del__(self):
-    #     print('some destructor')
-
     # Methods
     def displayData(self):
         print('Nazwa pliku: ', self.fncName)
@@ -31,7 +32,3 @@ class TaskFunction:
         for x_i in range(len(self.x)):
             print('Zakres x[', x_i, ']: ', self.x[x_i])
         print('Komentarz wyniku: ', self.scoreComment)
-    
-    # def Get(self, amount):
-    #     print('Kokos')
-    #     # self.__Balance -= amount
