fa28e56f18a42142fcfbcee7bd4e4c70ac2449bf
Filippo011235 <fadamcewicz1@gmail.com>
2021-04-16 13:02:02 +0200

    IHS preparation

---
+++ IHS.py
@@ -0,0 +1,12 @@
+from task_function import TaskFunction 
+
+class IHS:
+    """ """
+    
+    def __init__(self, filePath):
+        print('init')
+    
+    # Methods
+    def JakasMetoda(self):
+        print('Data')
+        
